6c3e8c2f1175fcd277b16b11b68a713010092d9a
Sadab <sadabrahman275@gmail.com>
2022-11-09 21:45:30 +0600

    query for myreview

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ async function run() {
         });
 
         app.get('/servicedetails/:id', async (req, res) => {
+
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query);
@@ -39,6 +40,27 @@ async function run() {
         });
 
         //review api
+
+        app.get('/reviews', async (req, res) => {
+            console.log(req.query.email);
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        })
+
+        app.get('/reviews', async (req, res) => {
+            const query = {};
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        })
+
 
         app.post('/reviews', async (req, res) => {
             const review = req.body;
